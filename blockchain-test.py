85b51dd44f56039c14093d252493d6e513424d93
Tord.Monclair <tord.monclair@student.uib.no>
2023-09-02 15:22:29 +0200

    wrote tests for parts of the blockchain

---
+++ blockchain-test.py
@@ -0,0 +1,45 @@
+import unittest
+from blockchain import Blockchain
+import blockchain
+from hashlib import sha256
+from json import dumps
+
+class BlockChainTest(unittest.TestCase):
+    
+    def test_create_chain(self):
+        chain = Blockchain()
+        transactions_data = [{"Sender": "test", "Recipient": "test", "Value": "test"}]
+        blockchain.create_block(transactions_data, chain)
+        keys_test = {"transactions": "", "height": "", "timestamp": "", "prev_hash": "", "nonce": "", "hash": ""}
+        self.assertEqual(chain.get()[0].keys(), keys_test.keys(), "Block not created correctly")
+    
+    def test_proof_of_work(self):
+        chain = Blockchain()
+        transactions_data = [{"Sender": "test", "Recipient": "test", "Value": "test"}]
+        block = {
+        "transactions": transactions_data,
+        "height": len(chain.get()),
+        "timestamp": "test",
+        "prev_hash": blockchain.get_prev_hash(chain),
+        "nonce": 0,
+        }
+        proof, _ = blockchain.proof_of_work(block)
+        test_proof = blockchain.hash_block(block)
+        self.assertEqual(proof, test_proof, "Proof of Work should always return same hash for identical block")
+    
+    def test_hash_block(self):
+        chain = Blockchain()
+        transactions_data = [{"Sender": "test", "Recipient": "test", "Value": "test"}]
+        block = {
+        "transactions": transactions_data,
+        "height": len(chain.get()),
+        "timestamp": "test",
+        "prev_hash": blockchain.get_prev_hash(chain),
+        "nonce": 0,
+        }
+        my_hash = blockchain.hash_block(block)
+        correct_hash = sha256(dumps(block, sort_keys=True).encode('utf8')).hexdigest()
+        self.assertEqual(my_hash, correct_hash, "Sha-256 not implemented correctly")
+        
+if __name__ == "__main__":
+    unittest.main()
